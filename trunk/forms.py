040370f583c20eb23e797e199866c145ed80014d
fedor.tyurin <fedor.tyurin@3da375b8-c7c2-11dd-94fd-9de6169c3690>
2011-03-18 14:08:57 +0000

    Correct fields update

--- trunk/forms.py
+++ trunk/forms.py
@@ -6,27 +6,35 @@ from models import *
 
 class CDBForm(forms.Form):
     def __init__(self, template=None, tag=None, *args, **kwargs):
+	forms.Form.__init__(self, *args, **kwargs)
 	if template:
 	    self.template = template
 	elif tag:
 	    self.template = Template.objects.get(tag=tag)
 	fields = self.template.fields()
 	for field in fields:
+            widget = None
+            help_text = ""
+            for parameter in field.parameters():
+                if parameter.tag == "widget":
+                    if parameter.value == "textarea":
+                        widget = forms.Textarea(attrs={'style': 'width: 350px;'})
+                elif parameter.tag == "help_text":
+                    help_text = parameter.value
 	    if field.type == "B":
-		self.base_fields[field.tag] = forms.BooleanField(label=field.title, required=field.required)
+		self.fields[field.tag] = forms.BooleanField(label=field.title,
+                                                                 required=field.required,
+                                                                 help_text=help_text)
 	    elif field.type == "T":
-                widget = forms.TextInput(attrs={'style': 'width: 350px;'})
-		for parameter in field.parameters():
-		    if parameter.tag == "widget":
-                        if parameter.value == "textarea":
-                            widget = forms.Textarea(attrs={'style': 'width: 350px;'})
-		self.base_fields[field.tag] = forms.CharField(label=field.title, required=field.required, widget=widget)
+                if widget == None:
+                    widget = forms.TextInput(attrs={'style': 'width: 350px;'})
+		self.fields[field.tag] = forms.CharField(label=field.title, required=field.required, widget=widget)
 	    elif field.type == "C":
 		choices = [('', '-')]
 		for parameter in field.parameters():
 		    if parameter.tag == "choice":
 			choices.append((parameter.value, parameter.value))
-                self.base_fields[field.tag] = forms.ChoiceField(choices=choices, 
+                self.fields[field.tag] = forms.ChoiceField(choices=choices, 
                                                                 label=field.title, 
                                                                 required=field.required)
             elif field.type == "M":
@@ -34,16 +42,15 @@ class CDBForm(forms.Form):
 		for parameter in field.parameters():
 		    if parameter.tag == "choice":
 			choices.append((parameter.value, parameter.value))
-                self.base_fields[field.tag] = forms.MultipleChoiceField(choices=choices, 
+                self.fields[field.tag] = forms.MultipleChoiceField(choices=choices, 
                                                                         label=field.title, 
                                                                         widget=forms.CheckboxSelectMultiple,
                                                                         required=field.required)
 	    elif field.type == "E":
-		self.base_fields[field.tag] = forms.EmailField(label=field.title, required=field.required)
+		self.fields[field.tag] = forms.EmailField(label=field.title, required=field.required)
 	    elif field.type == "U":
                 widget = forms.TextInput(attrs={'style': 'width: 350px;'})
-		self.base_fields[field.tag] = forms.URLField(label=field.title, required=field.required, widget=widget)
-	forms.Form.__init__(self, *args, **kwargs)
+		self.fields[field.tag] = forms.URLField(label=field.title, required=field.required, widget=widget)
 
     def save(self, commit=True):
 	data = self.cleaned_data
